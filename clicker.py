26b44c7536333e4a6e52c6a61926af9f7bc5510d
Reece <38808457+ItzzOwo@users.noreply.github.com>
2022-09-09 23:45:05 +0100

    V1.1
    Fixed up the code

--- clicker.py
+++ clicker.py
@@ -1,12 +1,11 @@
 from pynput.mouse import Button, Controller
 import random
 import keyboard
-import threading
+import time
 z = input("Enter a keybind: ")
 mouse = Controller()
 while True:
     while keyboard.is_pressed(z):
-        event = threading.Event()
-        event.wait(random.uniform(0.095614621967175,0.085614621967175))
+        time.sleep(random.uniform(0.095614621967175,0.085614621967175))
         x = 2 if random.random() < 0.64 else 1
         mouse.click(Button.left, x)
